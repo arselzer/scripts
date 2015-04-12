129db930238e7cb52c6eed8a22ff024b2cbb2db4
Alexander Selzer <aselzer3@gmail.com>
2015-04-12 22:43:38 +0200

    updates backup and proxy scripts

--- backup.sh
+++ backup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} /* /run/media/as/ALEX/backup
+rsync -aAXv \
+  --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","home/as/.cache/mozilla/firefox/*"} \
+  /* /run/media/as/ALEX/backup
--- proxy.sh
+++ proxy.sh
@@ -4,6 +4,11 @@ proxy_host=${PROXY_HOST:-"ssh.alexselzer.com"}
 proxy_user=${PROXY_USER:-"proxy"}
 proxy_port=${PROXY_PORT:-"3128"}
 
+if killall -0 chromium; then
+  echo "chromium is already running."
+  exit 1
+fi
+
 if [[ $(nc -z localhost 3128; echo $?) -eq 1 ]]; then
   echo "proxy off"
   ssh -D $proxy_port -f -N "$proxy_user@$proxy_host"
